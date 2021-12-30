52bd1d62b24aeef7fd87aa405aa1e8dcfbabbedb
chenjixin <chenjixin@didiglobal.com>
2021-12-30 16:41:46 +0800

    master

---
+++ test.c
@@ -0,0 +1,18 @@
+/**
+ * @file test.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-12-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    printf("hello\n");
+}
